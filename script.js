1879f86ea87330441edd6ad5a527e35804365cbd
baylagas <balbino_a@hotmail.com>
2021-02-02 20:08:17 -0600

    created login functionality

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ function checkLogin(){
     var password = document.getElementById("passw").value;
 
     var userArray = JSON.parse(sessionStorage.getItem("wUserArray"));
-    alert("in session")
+    //alert("in form-> " + user);
+    //alert("in session-> " + userArray[0].user)
 
     if(user !== null && user!== "" ){
         if(password !== null && password !== ""){
 
             //login es correcto o esta aprobado
             //enviarme al dashboard
-            if(user ==="bal" && password==="123"){
+            //if(user ==="bal" && password==="123"){
+            var canLogin = checkLoginInfo(user, password, userArray);
+            if(canLogin === true){
                 window.location.href="dashboard.html";
             }else{
                 alert("user or password are not correct");
@@ -24,6 +27,15 @@ function checkLogin(){
         alert("user must not be empty");
     }
 
+}
+
+function checkLoginInfo(user, password, userArray){
+    for(var i=0; i<userArray.length; i++){
+        if(userArray[i].user===user && userArray[i].password === password){
+            return true;
+        }
+    }
+    return false;
 }
 
 function goToRegistration(){
